3a12f27236e6b1d03a63a5e431860aea3371bd59
adriellison <adriellisonki@gmail.com>
2019-10-03 07:14:32 -0300

    aula 01
    Curso extra de python aula 01

---
+++ 1-Semestre/Curso extra de python/002_entrada_de_dados.py
@@ -0,0 +1,9 @@
+x = int(input("Digite o primeiro número: "))
+y = int(input("Digite o segundo número: "))
+
+print("\nO resultado da soma é: ", x + y)
+print("O resultado da subtração é: ", x - y)
+print("O resultado da multiplicação é: ", x * y)
+print("O resultado da divisão real é: ", x / y)
+print("O resultado da divisão inteira é: ", x // y)
+print("O resultado da potência é: ", x ** y)
---
+++ 1-Semestre/Curso extra de python/003_soma_de_gauss.py
@@ -0,0 +1,5 @@
+print("Soma de Gauss")
+#k = (n * (n + 1)) // 2
+n = int(input("Digite o valor n: "))
+k = (n * (n + 1)) // 2
+print(k) 
